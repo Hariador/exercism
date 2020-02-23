d497c244bb4a1784fc33b418b7a3bda3fb166c6e
Jonathan Reichert <jonathan.reichert@gmail.com>
2020-02-22 23:07:37 -0600

    Luhn check exercism completed. Weird how much passed while being totally wrong.

---
+++ ruby/luhn/luhn.rb
@@ -0,0 +1,28 @@
+=begin
+Write your code for the 'Luhn' exercise in this file. Make the tests in
+`luhn_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/luhn` directory.
+=end
+
+class Luhn 
+
+    def self.valid?(card_number)
+        
+        card_number = card_number.gsub(/[ ]/,'')
+        return false unless card_number =~ /^[0-9]*$/
+        return false unless card_number.length > 1
+        
+        
+
+        
+        digits = card_number.chars.map { |x| x.to_i}
+        i = digits.length - 2
+        while i >=0 do
+            digits[i] = digits[i].to_i*2 > 9 ? digits[i].to_i*2 -9 : digits[i].to_i*2
+            i = i -2
+        end
+       return digits.sum % 10 == 0
+    end
+end
